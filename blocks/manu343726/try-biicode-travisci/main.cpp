168ac26e4c0e5b459a0eaa4db394e093d10b7f31
Manu343726 <Manu343726@gmail.com>
2014-12-09 12:19:18 +0100

    Do some changes to test deploy

--- blocks/manu343726/try-biicode-travisci/main.cpp
+++ blocks/manu343726/try-biicode-travisci/main.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     std::cout << "hello TravisCI!" << std::endl;
+    std::cout << "We are biicode, testing your automated deploy!" << std::endl;
 }
 //Some changes...
 //Some changes...
